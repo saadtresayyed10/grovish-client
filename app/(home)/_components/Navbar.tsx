6f6dba642b0d0bdd0ce2f0f89cccfd19334ccc5a
Saad Sayyed <saadsyed950@gmail.com>
2024-12-29 19:42:19 +0530

    Mobile Menu added

--- app/(home)/_components/Navbar.tsx
+++ app/(home)/_components/Navbar.tsx
@@ -1,3 +1,4 @@
+import { Menu } from "lucide-react";
 import Link from "next/link";
 
 const Links = [
@@ -39,11 +40,32 @@ const HomeNavbar = () => {
       <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
         <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
         <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-6 py-1.5 text-base font-medium text-white backdrop-blur-3xl">
-          Start for Free
+          Book for Free
         </span>
       </button>
     </div>
   );
 };
 
-export { HomeNavbar };
+const HomeNavbarMobile = () => {
+  return (
+    <div className="flex lg:hidden justify-between items-center px-8 py-3 bg-black text-white font-roobert">
+      <Link href="/">
+        <h1 className="font-semibold text-4xl">Grovish</h1>
+      </Link>
+      <div className="flex items-center gap-x-4">
+        <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+          <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+          <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+            Book for Free
+          </span>
+        </button>
+        <button>
+          <Menu />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export { HomeNavbar, HomeNavbarMobile };
--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,10 +1,11 @@
-import { HomeNavbar } from "./_components/Navbar";
+import { HomeNavbar, HomeNavbarMobile } from "./_components/Navbar";
 
 const HomeLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <header>
+      <header className="w-full sticky top-0 z-50">
         <HomeNavbar />
+        <HomeNavbarMobile />
       </header>
       <main>{children}</main>
       <footer></footer>
