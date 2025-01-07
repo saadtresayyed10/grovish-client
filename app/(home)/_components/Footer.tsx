1614190f5167864c0cb4605a1b498b6604d20067
Saad Sayyed <saadsyed950@gmail.com>
2025-01-07 14:17:21 +0530

    Footer for mobile done

--- app/(home)/_components/Footer.tsx
+++ app/(home)/_components/Footer.tsx
@@ -48,18 +48,18 @@ const socials = [
 
 const Footer = () => {
   return (
-    <div className="lg:flex hidden justify-center items-center flex-col w-full">
-      <div className="flex justify-between items-center bg-gradient-to-tr from-violet-300 to-white text-black font-roobert w-full py-16 px-6">
-        <div className="flex justify-center items-center flex-col gap-y-4">
+    <div className="flex justify-center items-center flex-col w-full">
+      <div className="flex lg:justify-between lg:items-center flex-col lg:flex-row bg-gradient-to-tr from-violet-300 to-white text-black font-roobert w-full py-16 px-6">
+        <div className="flex justify-start items-start flex-col lg:gap-y-4 gap-y-2">
           <h1 className="text-6xl font-bold capitalize">Grovish</h1>
-          <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 ml-10">
+          <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 lg:ml-10 ml-6">
             <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
             <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-8 py-0.5 text-sm font-medium text-white backdrop-blur-3xl">
               Start Now
             </span>
           </button>
         </div>
-        <div className="flex justify-center items-center gap-x-28 mr-52">
+        <div className="flex justify-start items-start lg:gap-x-28 gap-y-10 lg:mr-52 lg:ml-0 ml-8 lg:mt-0 mt-10 flex-col lg:flex-row">
           <AboutFooter />
           <Legal />
           <Social />
