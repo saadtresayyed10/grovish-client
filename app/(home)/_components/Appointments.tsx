d3884bbcb767478892a951d41ca10d3202affa5d
Saad Sayyed <saadsyed950@gmail.com>
2025-01-08 14:00:13 +0530

    Appointment done for PC

---
+++ app/(home)/_components/Appointments.tsx
@@ -0,0 +1,24 @@
+import { FlipWords } from "@/components/ui/flip-words";
+
+const words = ["307", "308", "312", "317", "320", "324", "323", "327"];
+
+const AppointmentsText = () => {
+  return (
+    <div className="flex justify-center items-center flex-col w-full bg-black">
+      <div className="flex justify-center items-center">
+        <p className="text-violet-500 text-center font-roobert lg:text-2xl">
+          1, 28,
+        </p>
+        <FlipWords
+          words={words}
+          className="text-violet-500 text-center font-roobert lg:text-2xl"
+        />
+      </div>
+      <p className="text-violet-500 text-center font-roobert lg:text-xl">
+        Total Appointments booked today
+      </p>
+    </div>
+  );
+};
+
+export default AppointmentsText;
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,11 @@
+import AppointmentsText from "./_components/Appointments";
 import Hero from "./_components/Hero";
 
 const HomePage = () => {
   return (
-    <div className="flex justify-center items-center flex-col gap-y-10 bg-black w-full min-h-screen">
+    <div className="flex justify-center items-center flex-col gap-y-6 bg-black w-full min-h-screen">
       <Hero />
+      <AppointmentsText />
     </div>
   );
 };
