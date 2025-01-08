50434ad139ac6b282080c1a8a53d9f311e971527
Saad Sayyed <saadsyed950@gmail.com>
2025-01-08 17:25:48 +0530

    Making carousel work

---
+++ app/(home)/_components/Carousel.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CarouselComp = () => {
+  return <div className="flex justify-center items-center lg:mt-4 mt-1"></div>;
+};
+
+export default CarouselComp;
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,5 +1,4 @@
 import AppointmentsText from "./_components/Appointments";
-import { CarouselCard } from "./_components/CarouselCard";
 import Hero from "./_components/Hero";
 
 const HomePage = () => {
@@ -7,9 +6,6 @@ const HomePage = () => {
     <div className="flex justify-center items-center flex-col gap-y-5 bg-black w-full min-h-screen">
       <Hero />
       <AppointmentsText />
-      <div className="flex justify-center items-center lg:mt-4 mt-1">
-        <CarouselCard />
-      </div>
     </div>
   );
 };
