5a5f1c48648c8e2a16adec0bca087bd88e60af32
unknown <saadsyed950@gmail.com>
2025-01-15 19:21:44 +0530

    Working on booking form

--- app/(booking)/booking/[id]/layout.tsx
+++ app/(booking)/booking/[id]/layout.tsx
@@ -3,7 +3,7 @@ import BookingNavbar from "../_components/BookingNavbar";
 const BookingLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
-      <header>
+      <header className="top-0 sticky z-50 w-full">
         <BookingNavbar />
       </header>
       <main>{children}</main>
