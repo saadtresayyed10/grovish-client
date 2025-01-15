9e3794279a7d390e4f25cac64dd1a2a80e23e1ca
unknown <saadsyed950@gmail.com>
2025-01-15 22:08:08 +0530

    Map container done

--- app/(booking)/booking/_components/About.tsx
+++ app/(booking)/booking/_components/About.tsx
@@ -6,9 +6,19 @@ interface AboutProps {
 
 const About = ({ mapLocation }: AboutProps) => {
   return (
-    <div className="flex justify-center items-center w-full flex-col lg:p-10">
-      <div className="flex justify-center items-center gap-x-10">
+    <div className="flex justify-center items-center w-full flex-col p-10 gap-y-10 bg-white text-black font-roobert">
+      <div className="flex lg:justify-start lg:items-start justify-center items-center lg:flex-row flex-col gap-y-10 gap-x-10">
         <Maps mapLocation={mapLocation} />
+        <div className="flex lg:justify-start lg:items-start justify-center items-center flex-col">
+          <h4 className="lg:text-2xl text-lg font-medium capitalize">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. In magni
+            unde dolor ratione, nam aspernatur nihil quis distinctio quidem eos,
+            earum ipsum similique sequi enim. Et quam vel impedit saepe esse
+            minima tempore. Blanditiis quos architecto laudantium totam minima
+            quod tenetur? Expedita, ea perspiciatis ipsam unde debitis facere?
+            Itaque, molestias?
+          </h4>
+        </div>
       </div>
     </div>
   );
--- app/(booking)/booking/_components/Maps.tsx
+++ app/(booking)/booking/_components/Maps.tsx
@@ -4,14 +4,24 @@ interface MapsProps {
 
 const Maps = ({ mapLocation }: MapsProps) => {
   return (
-    <div>
-      <iframe
-        width="425"
-        height="350"
-        src={mapLocation}
-        className="rounded-lg shadow-md border border-black"
-      ></iframe>
-    </div>
+    <>
+      <div className="lg:block hidden">
+        <iframe
+          width="425"
+          height="350"
+          src={mapLocation}
+          className="rounded-lg shadow-md border border-black"
+        ></iframe>
+      </div>
+      <div className="lg:hidden">
+        <iframe
+          width="370"
+          height="320"
+          src={mapLocation}
+          className="rounded-lg shadow-md border border-black"
+        ></iframe>
+      </div>
+    </>
   );
 };
 
