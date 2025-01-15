8b88a90b8134846e193d17684378ab80396fce2f
unknown <saadsyed950@gmail.com>
2025-01-15 21:45:31 +0530

    Map embedded and working perfectly fine

--- app/(booking)/booking/[id]/page.tsx
+++ app/(booking)/booking/[id]/page.tsx
@@ -23,7 +23,7 @@ const BookService = () => {
         timeOpen={item.timeOpen}
         title={item.title}
       />
-      <About mapLocation="" />
+      <About mapLocation={item.mapLocation} />
     </div>
   );
 };
