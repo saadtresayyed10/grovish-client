3c9b06ecb99f40a8c3ece154b8faa3ed502de928
unknown <saadsyed950@gmail.com>
2025-01-16 18:08:48 +0530

    Review Card component is ready

--- app/(home)/_components/ReviewCard.tsx
+++ app/(home)/_components/ReviewCard.tsx
@@ -6,23 +6,27 @@ import {
   CardHeader,
 } from "@/components/ui/card";
 
-interface ReviewCardProps {}
+interface ReviewCardProps {
+  name: string;
+  review: string;
+  date: string;
+  ratings: number;
+}
 
-const ReviewCard = () => {
+const ReviewCard = ({ date, name, ratings, review }: ReviewCardProps) => {
   return (
     <Card className="bg-gradient-to-tr from-white to-violet-300 text-black px-6 font-roobert">
       <div className=" flex justify-start items-start flex-col gap-y-1">
-        <CardHeader className="font-bold lg:text-2xl">Name</CardHeader>
+        <CardHeader className="font-bold lg:text-2xl">{name}</CardHeader>
         <CardContent className="lg:w-[92%]">
           <CardDescription className="text-black lg:text-base">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa,
-            deleniti.
+            {review}
           </CardDescription>
         </CardContent>
       </div>
       <CardFooter className="flex justify-start flex-col items-start lg:text-sm text-black">
-        <p>Date: 12/12/12</p>
-        <p>Ratings: 5</p>
+        <p>Date: {date}</p>
+        <p>Ratings: {ratings}</p>
       </CardFooter>
     </Card>
   );
