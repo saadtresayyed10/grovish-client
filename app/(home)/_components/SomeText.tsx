bd173669042b55fde63bbfac2fe6e14e17789903
unknown <saadsyed950@gmail.com>
2025-01-22 19:05:35 +0530

    Fixed some text

--- app/(home)/_components/SomeText.tsx
+++ app/(home)/_components/SomeText.tsx
@@ -3,8 +3,8 @@ const SomeText = () => {
     <div className="bg-black text-white font-roobert capitalize font-light text-left lg:py-20 lg:px-10 py-10  px-6">
       <h1 className="lg:text-6xl text-4xl">
         From <span className="font-bold text-violet-300">booking</span> to{" "}
-        <span className="font-bold text-violet-300">checkout</span>, use
-        GlossGenius for a 5-star experience that keeps your clients coming back.
+        <span className="font-bold text-violet-300">checkout</span>, use Grovish
+        for a 5-star experience that keeps your clients coming back.
       </h1>
     </div>
   );
