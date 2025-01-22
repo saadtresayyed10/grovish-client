6e74a364f17b0c0b9f2d7d64c32d97af6f5321f5
unknown <saadsyed950@gmail.com>
2025-01-22 13:35:33 +0530

    Features section is almost done for pc, only title is left

--- app/(home)/_components/Features.tsx
+++ app/(home)/_components/Features.tsx
@@ -3,7 +3,7 @@ import { FeaturesCardLG } from "./FeaturesCard";
 
 const Features = () => {
   return (
-    <div className="bg-white text-black w-full h-min flex justify-center items-center flex-col lg:gap-y-10 lg:p-10">
+    <div className="bg-white text-black w-full h-min flex justify-center items-center flex-col lg:gap-y-20 lg:p-10">
       {features.map((feature, idx) => (
         <FeaturesCardLG
           key={idx}
